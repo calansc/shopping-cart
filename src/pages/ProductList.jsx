10345b746df5d5f2b695c6a679abee49a24696b6
Chad Schroeder <77646640+calansc@users.noreply.github.com>
2024-06-07 16:13:38 -0400

    Update product list style

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -10,10 +10,8 @@ export default function ProductList({ onClickRed, onClickBlue, onClickGreen }) {
                 <li>
                     <Link to="/products/red"> 
                         <img className="red" src={cart} alt="Cart" height="50"/>
-                        <br/>
-                        Product Red
+                        <p>Product Red</p>
                     </Link>
-                    <br/>
                     <button onClick={onClickRed}>
                         Quick Add to Cart
                     </button>
@@ -22,10 +20,8 @@ export default function ProductList({ onClickRed, onClickBlue, onClickGreen }) {
                 <li>
                     <Link to="/products/blue"> 
                         <img className="blue" src={cart} alt="Cart" height="50"/>
-                        <br/>
-                        Product Blue
+                        <p>Product Blue</p>
                     </Link>
-                    <br/>
                     <button onClick={onClickBlue}>
                         Quick Add to Cart
                     </button>
@@ -33,14 +29,38 @@ export default function ProductList({ onClickRed, onClickBlue, onClickGreen }) {
                 </li>                <li>
                     <Link to="/products/green"> 
                         <img className="green" src={cart} alt="Cart" height="50"/>
-                        <br/>
-                        Product Green
+                        <p>Product Green</p>
                     </Link>
-                    <br/>
                     <button onClick={onClickGreen}>
                         Quick Add to Cart
                     </button>
-                   
+                </li>
+                <li>
+                    <Link to="/products/green"> 
+                        <img className="green" src={cart} alt="Cart" height="50"/>
+                        <p>Product 4</p>
+                    </Link>
+                    <button onClick={onClickGreen}>
+                        Quick Add to Cart
+                    </button>
+                </li>
+                <li>
+                    <Link to="/products/green"> 
+                        <img className="green" src={cart} alt="Cart" height="50"/>
+                        <p>Product 5</p>
+                    </Link>
+                    <button onClick={onClickGreen}>
+                        Quick Add to Cart
+                    </button>
+                </li>
+                <li>
+                    <Link to="/products/green"> 
+                        <img className="green" src={cart} alt="Cart" height="50"/>
+                        <p>Product 6</p>
+                    </Link>
+                    <button onClick={onClickGreen}>
+                        Quick Add to Cart
+                    </button>
                 </li>
             </ul>
             
